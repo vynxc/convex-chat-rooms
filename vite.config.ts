df8844ecfcbe6f7876dc3dc402aa5f141a203bae
vynxc <vynxcs@gmail.com>
2024-06-14 03:25:17 -0400

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+dotenv.config();
+const url = process.env.PUBLIC_CONVEX_URL!.replace('convex.cloud', 'convex.site');
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
 			'/convex': {
-				target: 'http://localhost:3211/',
+				target: url,
 				changeOrigin: true
 			}
 		}
