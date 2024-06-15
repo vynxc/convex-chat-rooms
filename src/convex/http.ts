e5b50cde2a72c7517bcfe53ab890b338d1a62736
vynxc <vynxcs@gmail.com>
2024-06-15 16:55:28 -0400

    clean imports

--- src/convex/http.ts
+++ src/convex/http.ts
@@ -1,12 +1,7 @@
 import { Hono } from 'hono';
-import { zValidator } from '@hono/zod-validator';
 import { HttpRouterWithHono, type HonoWithConvex } from 'convex-helpers/server/hono';
 import type { ActionCtx } from './_generated/server';
-import { api } from './_generated/api';
 import { cors } from 'hono/cors';
-import { generateState, OAuth2RequestError } from 'arctic';
-import { GitHub } from 'arctic';
-import z from 'zod';
 import { loginRoute } from './api/auth/login';
 import { registerRoute } from './api/auth/register';
 import { githubRoute } from './api/auth/github';
